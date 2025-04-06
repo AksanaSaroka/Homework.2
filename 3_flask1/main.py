ce5b3f8001397fa138d912f6068780dbabc5d8e3
Aksana <aksana.zholtko@mail.ru>
2025-04-06 21:05:12 +0300

    less3_end

--- 3_flask1/main.py
+++ 3_flask1/main.py
@@ -25,12 +25,16 @@ from flask import Flask, render_template
 import os
 import requests
 import random
+from pyowm import OWM
+from pprint import pprint
 
 BASE_DIR = os.path.dirname(__file__)
 
 app = Flask(__file__,
             static_folder=os.path.join(BASE_DIR, 'static'),
             template_folder=os.path.join(BASE_DIR, 'templates'))
+
+
 
 @app.route("/")
 def index():
@@ -47,9 +51,7 @@ def duck():
                            duck_photo_number = data['url'].split('/')[-1].split('.')[0] 
                            )
 
-
     
-
 @app.route("/fox/")
 @app.route("/fox/<int:count>/")
 def fox(count=1):
@@ -59,8 +61,8 @@ def fox(count=1):
             while len(foxes) != count:
                 response = requests.get('https://randomfox.ca/floof')
                 foxes.append(response.json()['image'])
-       
-            print(foxes)
+
+            # print(foxes)
             return render_template ('fox.html',
                                 foxes = foxes,
                                 count_fox = count,
@@ -72,11 +74,43 @@ def fox(count=1):
 
     except Exception as e:
         return render_template ('fox.html',
-                                text = f"Oшибка")
+                                text = "Oшибка")
+    
+
+@app.route("/weather/")
+@app.route("/weather/<string:city>/")
+def weather_city(city='Minsk'):
+    try:
+        owm = OWM('87b7c330daf2579649274a648d33f753')
+        manager = owm.weather_manager()
+        obs = manager.weather_at_place(city)
+        weather = obs.weather
+        # pprint(obs.to_dict())
+        weather_data = {
+            'city' : city,
+            'status': weather.status,
+            'humidity': weather.humidity,
+            'temp': weather.temp['temp'],
+            'feels_like': weather.temp['feels_like'],
+        }
+
+        return render_template('weather_city.html',
+                           city=city,
+                           weather = weather_data,
+                           temp = round((weather.temp['temp'] - 273.5),1),
+                           feels_like = round((weather.temp['feels_like'] - 273.5),1)
+                           )
+    
+    except Exception as e:
+        return render_template ('weather_city.html',
+                                text = "Oшибка")
+    
+@app.route("/weather-minsk/")
+def weather_minsk():
+    return weather_city(city='Minsk')
+
 
 # Сработает если ошибка 404 - т.е. любой другой путь который выше не предусмотрен
-
-
 @app.errorhandler(404)
 def page_not_found(error):
     return '<h1 style="color:red">такой страницы не существует</h1>'
